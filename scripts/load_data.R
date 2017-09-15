8e7285594dfad5577ab8337dba00eabad5da3403
Conor Tompkins <conorotompkins@gmail.com>
2017-09-15 12:53:18 +0000

    moved script

--- scripts/load_data.R
+++ scripts/load_data.R
@@ -38,23 +38,3 @@ df %>%
 
 read_csv("data/playoff_seeding/20162017", skip = 2)
 ?read_csv
-
-rm(list = ls())
-data_path <- "./data/playoff_seeding"
-data_list <- list.files(path = data_path)
-?list.files
-data_list <- paste0(data_path, "/", data_list)
-data_list <- lapply(data_list, read_csv)
-
-#for (i in seq_along(data_list)) {
-#  data_list[[i]]$season <- levels_season[[i]]
-#}
-
-
-
-playoff_seeding <- bind_rows(data_list)
-
-df_seasons <- tibble(season = levels_season)
-
-colnames() <- tolower(colnames(data))
-colnames(data) <- gsub(" ", "_", colnames(data))
