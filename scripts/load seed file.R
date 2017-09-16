289e8abd238143ec2b0c1260df66964bb75b7f7c
Conor Tompkins <conorotompkins@gmail.com>
2017-09-16 21:57:19 +0000

    got list names used as the first column

--- scripts/load seed file.R
+++ scripts/load seed file.R
@@ -1,26 +1,29 @@
+library(stringr)
+
 rm(list = ls())
+
 data_path <- "./data/playoff_seeding"
+
 data_list <- list.files(path = data_path)
+
+seasons <- paste(data_list)
+
 data_list <- paste0(data_path, "/", data_list)
+
 data_list <- lapply(data_list, read_csv)
 
-#for (i in seq_along(data_list)) {
-#  data_list[[i]]$season <- levels_season[[i]]
-#}
+
+names(data_list) <- seasons
+
+names(data_list)
 
 for (i in seq_along(data_list)) {
-  data_list[[i]] <- data_list[[i]][3:33, 1:2]
+  data_list[[i]] <- data_list[[i]][3:33, c(1, 2, 8)]
 }
 
-for(i in seq_along(data_list)) {
-  colnames(data_list[[i]]) <- c("rank", "team")
-}
+data_list[1]
 
-
-team_seeds <- bind_rows(data_list) %>% 
-  filter(team != "League Average")
-
-df_seasons <- tibble(season = rep(levels_season, each = 30))
-
-team_seeds <- bind_cols(team_seeds, df_seasons)
-
+team_seeds <- bind_rows(data_list, .id = "season") %>% 
+  rename(rank = X1,
+         team = X2,
+         points = X8)
