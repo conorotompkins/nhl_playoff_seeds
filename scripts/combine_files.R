21b6f31e606201eabbbf1dbb5565f7cb230895d0
Conor Tompkins <conorotompkins@gmail.com>
2017-09-17 17:49:36 +0000

    combined files, added graph code

--- scripts/combine_files.R
+++ scripts/combine_files.R
@@ -1,9 +1,11 @@
 library(tidyverse)
 library(viridis)
 
+set.seed(1234)
+
 rm(list = ls())
 
-source("scripts/load_data.R")
+source("scripts/load_results_data.R")
 source("scripts/load_seed_data.R")
 
 df_team_abbr <- read_csv("data/NHL_team_names_abbr.csv")
@@ -16,16 +18,35 @@ df
 df_seeds
 
 df <- df %>% 
-  left_join(df_results) %>% 
-  filter(season != "20052006", season != "20062007")
+  left_join(df_results)
 df
 
 df %>% 
   count(rank, wins) %>% 
+  filter(!is.na(wins)) %>% 
   ggplot(aes(rank, wins, fill = n)) +
   geom_tile() +
+  geom_smooth() +
   scale_fill_viridis() +
-  scale_x_continuous(expand = c(0,0), breaks = 1:16) +
-  scale_y_continuous(expand = c(0,0)) +
+  #coord_cartesian(xlim = c(0, 21)) +
+  scale_x_continuous(expand = c(0,0), breaks = 1:20) +
+  scale_y_continuous(expand = c(0,0), breaks = c(0, 4, 8, 12, 16)) +
   coord_equal() +
   theme(panel.grid = element_blank())
+
+df %>% 
+  filter(!is.na(wins)) %>% 
+  ggplot(aes(rank, wins)) +
+  geom_jitter(height = .1, width = .1, alpha = .3) +
+  geom_smooth() +
+  scale_x_continuous(breaks = 1:20) +
+  scale_y_continuous(breaks = c(0, 4, 8, 12, 16)) + 
+  theme(panel.grid = element_blank())
+
+df %>% 
+  filter(season != "20122013") %>% 
+  ggplot(aes(points, wins)) + 
+  geom_point(alpha = .3) +
+  geom_smooth() +
+  scale_y_continuous(breaks = c(0, 4, 8, 12, 16)) +
+  coord_cartesian(xlim = c(85, 130))
