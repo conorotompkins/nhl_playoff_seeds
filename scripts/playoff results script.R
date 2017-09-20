3b427c63c2f374a1c179abdcdb9df21465cc8f70
Conor Tompkins <conorotompkins@gmail.com>
2017-09-20 01:36:24 +0000

    updated script

--- scripts/playoff results script.R
+++ scripts/playoff results script.R
@@ -16,4 +16,24 @@ names(data_list) <- seasons
 
 names(data_list)
 
+for (i in seq_along(data_list)) {
+  x <- nrow(data_list[[i]])
+  data_list[[i]] <- data_list[[i]][2:x,]
+}
+
+
+
 df_playoff_results <- bind_rows(data_list, .id = "season")
+
+colnames(df_playoff_results) <- tolower(colnames(df_playoff_results))
+  
+df_playoff_results <- df_playoff_results %>% 
+  rename(rank = rk,
+         wins = w,
+         losses = l,
+         ties = t,
+         wins_overtime = ow,
+         losses_overtime = ol,
+         win_percentage = `w-l%`,
+         gf = g) %>% 
+  filter(team != "League Average")
